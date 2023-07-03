1df060181ceda86905e5db0b78188fd34413298a
dongyubin <892457803@qq.com>
2023-07-03 10:50:37 +0800

    将评论、点赞、打赏的js分离

--- WP/js/ajax.js
+++ WP/js/ajax.js
@@ -1,19 +1,4 @@
-jQuery(document).ready(function($) {
-    $(".pay_item").click(function(){
-		$(this).addClass('checked').siblings('.pay_item').removeClass('checked');
-		var dataid=$(this).attr('data-id');
-		$(".shang_payimg img").attr("src","https://p.wangdu.site/images/"+dataid+".png");
-		$("#shang_pay_txt").text(dataid=="alipay"?"支付宝":"微信");
-	});
-	$('.dashang').click(function() {
-	    $(".hide_box").fadeToggle();
-	    $(".shang_box").fadeToggle();
-	})
-	$('.shang_close').click(function() {
-	    $(".hide_box").fadeToggle();
-	    $(".shang_box").fadeToggle();
-	})
-});
+// 分页
 jQuery(function($){
 	
 	$('.xintheme-loadmore').click(function(){
@@ -79,11 +64,7 @@ jQuery(function($){
 						if( data ) {
 							$('.posts-wrapper').find('article:last-of-type').after( data ); // 在哪个位置插入文章
 							canBeLoaded = true; // AJAX已经完成，现在我们可以再次运行它了
-
-
-
 							xintheme.current_page++;
-
 							if (xintheme.current_page >= xintheme.max_page ) 
 								button.remove(); // 如果最后一页，删除按钮
 						}
@@ -92,71 +73,6 @@ jQuery(function($){
 			}
 		});
 	}
-
-	// 点赞
-	$(".like").click(function() {
-		if ($(this).hasClass('liked')) {
-			alert('您已经赞过了~');
-		} else {
-			$(this).addClass('liked');
-
-			var button = $(this);
-		
-			$.ajax({
-				url : xintheme.ajaxurl, // AJAX处理程序
-				data : {
-					action: "post_like",
-					post_id: $(this).data("id")
-				},
-				type : 'POST',
-				success : function( data ){
-					button.children('.count').html(data);
-				}
-
-			});
-		}
-
-		return false;
-	});
-	
-	$("#qqinfo").blur(function(){
-	    var qq_num1=$('#qqinfo').val();
-	    var qq_num = $.trim(qq_num1);
-        if(qq_num.length>5 || 10<qq_num.length){
-            if(!isNaN(qq_num)){
-            $.ajax({
-            url:"https://www.wangdu.site/get_qq_info.php",    //如果网站开启了HTTPS，记得这里要修改成https的链接
-            type:"get",
-            data:{qq:qq_num},
-            dataType:"json",
-                success:function(data){
-                    $("#email").val(qq_num+'@qq.com');
-                    $('#comment').focus();
-                    if(data==null){
-                    $("#author").val('QQ游客'); 
-                    }else{
-                    $("#author").val(data[qq_num][6]==""?'QQ游客':data[qq_num][6]);
-                    } 
-                },
-                error:function(err){
-                    $("#author").val('QQ游客');
-                    $("#email").val(qq_num+'@qq.com');
-                    $('#comment').focus();
-                }
-            });
-            }else{
-                $("#qqinfo").val('');
-                $("#qqinfo").attr('placeholder',"QQ号不正确,请再输入！");
-                $("#author").val('');
-                $("#email").val('');
-            } 
-        }else{
-             $("#qqinfo").val('');
-             $("#qqinfo").attr('placeholder',"QQ号不正确,请再输入！");
-             $("#author").val('');
-             $("#email").val('');
-        }
-	});
 
 	//投稿
 	$(document).on('click', '.publish_post', function(event) {
